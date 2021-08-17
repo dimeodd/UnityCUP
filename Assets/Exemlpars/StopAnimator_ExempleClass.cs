a3396c6a68f1953cd3a5df2a74fd9d967918557d
4DO <60938162+dimeodd@users.noreply.github.com>
2021-08-18 00:01:43 +0300

    Update StopAnimator_ExempleClass.cs

--- Assets/Exemlpars/StopAnimator_ExempleClass.cs
+++ Assets/Exemlpars/StopAnimator_ExempleClass.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class StopAnimator_ExempleClass : MonoBehaviour
 {
+  [SerializeField] Animator rig;
 
+    protected override void ChronoStart()
+    {
+        // Connect Animator to Pause
+        InitBehaviour(rig);
+    }
 }
